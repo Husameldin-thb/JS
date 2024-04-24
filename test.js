9f7ad9d6df2d66fffd52c7386b941e409a28a3fe
Husameldin-thb <osman@th-brandenburg.de>
2024-04-24 14:10:01 +0300

    Test

---
+++ test.js
@@ -0,0 +1,151 @@
+document.querySelector('h1').style.color ='blue'
+console.log('Hosam');
+console.error('test');
+console.log(typeof(1))
+console.log (typeof('hosam'))
+console.log (typeof(true))
+//window.alert('hallo hosam');
+document.querySelector('h2').style.color= 'red'
+console.log (5+5);
+
+
+// Number 
+console.log(Number.isInteger('233'));
+console.log(Number('34')+ Number('65'));
+
+// Math:
+console.log(Math.floor(234.34));
+console.log(Math.round(234.34));
+console.log(Math.ceil(34.3445));
+console.log(Math.max(34,35,465,67,67,));
+console.log(Math.min(3234,46,6,776,432));
+
+/*let name = 'Hosameldin Osman'
+let age = 31 
+console.log('my name is '+ name + ' and iam ' + age + ' Old' );
+name = window.prompt('enter your first name');
+console.log(name);
+let name1 =  window.prompt('enter your last name');
+console.log(name1);
+age = window.prompt('enter your age please');
+console.log(age); */
+//location.reload(true);
+
+
+/*let zakah = window.prompt()
+console.log(0.025* zakah + ' SDG') ;
+
+let name = window.prompt('enter your first name');
+console.log('wilcome ' + name);
+
+console.log(Number.isInteger(25));
+console.log(Number.isInteger('SDG'));
+console.log(typeof('SDG'));
+
+//location.reload(); */
+
+//console, doc, window
+console.log('Husam');
+console.error('Test2');
+//window.alert('Above 18?');
+document.write('Test 3');
+
+// write two Things together
+let x = 'Fahrschule';
+let y = 5;
+console.log ('die ' + x + 'number '+ y  );
+
+//number
+console.log(Number.isInteger());
+console.log (Number('54') + Number('54'));
+
+
+//math 
+console.log(Math.max(21,23,4,5,6567));
+console.log(Math.floor(43.5));
+console.log(Math.ceil(43.55));
+console.log(Math.round(34.45));
+
+
+//String:
+x = 'Husameldin Osman Awad Elkarim';
+y = 31 
+
+console.log (x.length);
+console.log (x[2]);
+console.log (x.indexOf('u'))
+console.log(x.includes('Husam'));
+console.log( typeof String(y));
+console.log (x.split('_'));
+console.log (x.search('Osman'));
+
+
+
+// Number: //Math 
+console.log(Number.isInteger);
+console.log (Math.max(234,345,56));
+
+// String:
+
+let name2 = 'Husameldin ibn Osman ibn Awad'
+
+console.log(name2.repeat(3));
+console.log (name2.length);
+console.log (name2[2]);
+console.log(name2.includes('Husma'));
+console.log(name2.search('Husam'));
+console.log(name2.split(' '));
+console.log(name2.indexOf('u'));
+
+
+
+console.log (name2.indexOf('husam'));
+console.log(name2.includes('Husam'));
+
+
+
+console.log ('Test'); 
+
+console.log(typeof(true));
+
+/*name2= window.prompt('enter your first name');
+console.log(name2);
+let age = window.prompt('enter your age please');
+console.log (age); */
+
+
+window.document.querySelector('h3').style.color='red';
+document.write('what is any thing');
+console.error('error');
+
+console.log(typeof('was'));
+console.log (typeof(2));
+console.log (typeof (null));
+console.log (typeof (true, false));
+
+// Number
+// change string to number:
+console.log (Number('34')+ Number('54'));
+// check if is a number:
+console.log (Number.isInteger(232));
+console.log(Number.MAX_SAFE_INTEGER);
+console.log (Number.isSafeInteger(23.33));
+
+console.log(Math.ceil(23.423));
+console.log (Math.max(23,23.435,543));
+
+/*let firstname = 'Husam';
+firstname = window.prompt ('enter your first name');
+let lastname =  window.prompt('enter your last name');
+console.log ('hallo '+ firstname + ' ' + lastname);
+let age = window.prompt ('enter your age');
+window.alert  ('you are above 18 and your age is ' +  age);
+*/
+
+let name3 = 'Husameldin Osman Awad';
+console.log (name3.indexOf('H'));
+console.log (name3.length);
+console.log(name3[2]);
+console.log(name3.repeat(3));
+console.log (name3.includes('1'));
+
